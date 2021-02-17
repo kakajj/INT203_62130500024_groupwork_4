a558e1e8ff3f2bb6b83361d8c603d5617c5994c8
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-02-17 12:17:08 +0700

    [update] fix picture allignment

--- 62130500024_groupwork_4/main.js
+++ 62130500024_groupwork_4/main.js
@@ -2,9 +2,9 @@ const app = {
     data(){
         return{
             pics:[
-                {name:"Art #1",url:"https://images.unsplash.com/photo-1612897747445-6849b81bab9e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",like:false},
-                {name:"Art #2",url:"https://images.unsplash.com/photo-1612870963422-b5cab65a0547?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=645&q=80",like:false},
-                {name:"Art #3",url:"https://images.unsplash.com/photo-1612864800594-22b1b1c44de8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=712&q=80",like:false}
+                {name:"The Shoes",url:"https://images.unsplash.com/photo-1612897747445-6849b81bab9e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",like:false},
+                {name:"Snowing Day",url:"https://images.unsplash.com/photo-1612870963422-b5cab65a0547?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=645&q=80",like:false},
+                {name:"Northern lights",url:"https://images.unsplash.com/photo-1612864800594-22b1b1c44de8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=712&q=80",like:false}
             ],
             searchField:'',
             isSearch: true,
