5e53030c6e80ea34fb1289ff55f0bbbd45e28ee8
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-02-17 10:56:02 +0700

    [update] search function

--- 62130500024_groupwork_4/main.js
+++ 62130500024_groupwork_4/main.js
@@ -1,21 +1,36 @@
 const app = {
     data(){
         return{
-            pics:[{name:"Art #1",url:"https://images.unsplash.com/photo-1612897747445-6849b81bab9e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",like:false},
+            pics:[
+                {name:"Art #1",url:"https://images.unsplash.com/photo-1612897747445-6849b81bab9e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",like:false},
                 {name:"Art #2",url:"https://images.unsplash.com/photo-1612870963422-b5cab65a0547?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=645&q=80",like:false},
                 {name:"Art #3",url:"https://images.unsplash.com/photo-1612864800594-22b1b1c44de8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=712&q=80",like:false}
-            ]
+            ],
+            searchField:'',
+            isSearch: true,
         }
     },
     computed:{
         picCount(){
             return this.pics.filter(n => n.like).length;
-        }
+        },
+        filteredList(){
+            return this.pics.filter(pic => {
+                return pic.name.toLowerCase().includes(this.searchField.toLowerCase())
+              });
+        },
     },
     methods:{
         likePic(index){
             this.pics[index].like = !this.pics[index].like;
-        }
+        },
+        startSearch(){
+            this.isSearch = !this.isSearch;
+        },
+        cancelSearch(){
+            this.isSearch = !this.isSearch;
+            this.searchField = '';
+        },
     }
 };
 
