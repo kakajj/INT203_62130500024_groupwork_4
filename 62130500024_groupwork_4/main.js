31fef6cfcc956a466920072326e70b652f665ba2
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-02-19 00:12:30 +0700

    [update] Big Update
    แก้ UI ใหม่ทั้งหมด รื้อโครงใหม่ จัด grid ใหม่สวยงาม เพิ่มฟังก์ชั่นกดหัวใจแล้วเปลี่ยนสี เพิ่มเอฟเฟ็ค Hover

--- 62130500024_groupwork_4/main.js
+++ 62130500024_groupwork_4/main.js
@@ -2,10 +2,16 @@ const app = {
     data(){
         return{
             pics:[
-                {name:"The Shoes",url:"https://images.unsplash.com/photo-1612897747445-6849b81bab9e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",like:false},
-                {name:"Snowing Day",url:"https://images.unsplash.com/photo-1612870963422-b5cab65a0547?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=645&q=80",like:false},
-                {name:"Northern lights",url:"https://images.unsplash.com/photo-1612864800594-22b1b1c44de8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=712&q=80",like:false}
+                {name:"Rose LoveSickGirl",url:"./images/1.jpg",like:false},
+                {name:"Lisa LoveSickGirl",url:"./images/2.jpg",like:false},
+                {name:"Jennie LoveSickGirl",url:"./images/3.jpg",like:false},
+                {name:"Jisoo LoveSickGirl",url:"./images/4.jpg",like:false},
+                {name:"Rose HowYouLikeThat",url:"./images/h3.jpg",like:false},
+                {name:"Lisa HowYouLikeThat",url:"./images/h4.jpg",like:false},
+                {name:"Jennie HowYouLikeThat",url:"./images/h1.jpg",like:false},
+                {name:"Jisoo HowYouLikeThat",url:"./images/h2.jpg",like:false}
             ],
+            heart:0,
             searchField:'',
             isSearch: true,
         }
@@ -22,6 +28,7 @@ const app = {
     },
     methods:{
         likePic(index){
+            this.heart++;
             this.pics[index].like = !this.pics[index].like;
         },
         startSearch(){
