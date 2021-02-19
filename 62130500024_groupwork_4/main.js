70c82b8a5cb58b9ded68c24c908dbd7fe7f6958b
Nachanon Montikanon <nachanon.montikanon@mail.kmutt.ac.th>
2021-02-19 21:44:16 +0700

    [update] Modal Image

--- 62130500024_groupwork_4/main.js
+++ 62130500024_groupwork_4/main.js
@@ -2,23 +2,31 @@ const app = {
     data(){
         return{
             pics:[
-                {name:"Rose LoveSickGirl",url:"./images/1.jpg",like:false},
-                {name:"Lisa LoveSickGirl",url:"./images/2.jpg",like:false},
-                {name:"Jennie LoveSickGirl",url:"./images/3.jpg",like:false},
-                {name:"Jisoo LoveSickGirl",url:"./images/4.jpg",like:false},
-                {name:"Rose HowYouLikeThat",url:"./images/h3.jpg",like:false},
-                {name:"Lisa HowYouLikeThat",url:"./images/h4.jpg",like:false},
-                {name:"Jennie HowYouLikeThat",url:"./images/h1.jpg",like:false},
-                {name:"Jisoo HowYouLikeThat",url:"./images/h2.jpg",like:false}
+                {name:"Rose LoveSickGirl",url:"./images/1.jpg",     like:false,showModal:false},
+                {name:"Lisa LoveSickGirl",url:"./images/2.jpg",     like:false,showModal:false},
+                {name:"Jennie LoveSickGirl",url:"./images/3.jpg",   like:false,showModal:false},
+                {name:"Jisoo LoveSickGirl",url:"./images/4.jpg",    like:false,showModal:false},
+                {name:"Rose HowYouLikeThat",url:"./images/h3.jpg",  like:false,showModal:false},
+                {name:"Lisa HowYouLikeThat",url:"./images/h4.jpg",  like:false,showModal:false},
+                {name:"Jennie HowYouLikeThat",url:"./images/h1.jpg",like:false,showModal:false},
+                {name:"Jisoo HowYouLikeThat",url:"./images/h2.jpg", like:false,showModal:false}
             ],
-            heart:0,
+            heart:["heart"],
             searchField:'',
             isSearch: true,
+            
         }
     },
     computed:{
         picCount(){
             return this.pics.filter(n => n.like).length;
+        },
+        heartCount(){
+            if(this.heart.length<1){
+                this.heart.length=0;
+                return this.heart;
+            }
+            return this.heart;
         },
         filteredList(){
             return this.pics.filter(pic => {
@@ -28,8 +36,13 @@ const app = {
     },
     methods:{
         likePic(index){
-            this.heart++;
-            this.pics[index].like = !this.pics[index].like;
+            if(this.pics[index].like == false){
+                this.heart.push("💖");
+                this.pics[index].like = !this.pics[index].like;
+            }else{
+                this.heart.pop();
+                this.pics[index].like = !this.pics[index].like;
+            }
         },
         startSearch(){
             this.isSearch = !this.isSearch;
@@ -38,6 +51,9 @@ const app = {
             this.isSearch = !this.isSearch;
             this.searchField = '';
         },
+        toggleModal(index){
+            this.pics[index].showModal = !this.pics[index].showModal;
+          }
     }
 };
 
